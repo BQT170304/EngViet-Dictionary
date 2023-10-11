127f7b0e265bf7a96e2dbef347db6c34ad5e6ce1
qwerty1928375 <baitaplon888@gmail.com>
2023-10-11 23:42:38 +0700

    UI
    dictionary UI

--- Dictionary/src/main/java/com/example/dictionary/Main.java
+++ Dictionary/src/main/java/com/example/dictionary/Main.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("interface.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Dictionary.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
